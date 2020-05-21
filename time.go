ddd2370072fe7d98a0e031f07d1a36af23c8b1b3
xudong <834971685@qq.com>
2020-05-21 17:00:31 +0800

    格式化字符串转unix时间戳

--- time.go
+++ time.go
@@ -1,8 +1,8 @@
-package time
+package Assist
 
 import "time"
 
-func FormatToUnix(t string) int64 {
+func TimeFormatToUnix(t string) int64 {
 	timeLayout := "2006-01-02 15:04:05"
 	loc, _ := time.LoadLocation("Local")
 	theTime, _ := time.ParseInLocation(timeLayout, t, loc) //使用模板在对应时区转化为time.time类型
