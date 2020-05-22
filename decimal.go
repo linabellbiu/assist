6e0a8bc0b64be3ed3ed7213961847e2069bd9f2b
xudong <834971685@qq.com>
2020-05-22 14:31:02 +0800

    增加浮点判断

--- decimal.go
+++ decimal.go
@@ -2,6 +2,7 @@ package assist
 
 import (
 	"github.com/shopspring/decimal"
+	"math"
 	"strconv"
 )
 
@@ -26,4 +27,43 @@ func StringToInt64(value string) (i int64) {
 func StringToInt(value string) (i int) {
 	i, _ = strconv.Atoi(value)
 	return
+}
+
+func Float64ToInt64(value float64) (i int64) {
+	i = decimal.NewFromFloat(value).IntPart()
+	return
+}
+
+func RoundFloat64(value float64, exp int32) (num float64) {
+	num, _ = decimal.NewFromFloat(value).Round(exp).Float64()
+	return
+}
+
+var A Accuracy = func() float64 { return 0.00000001 }
+
+type Accuracy func() float64
+
+//=
+func (this Accuracy) Equal(a, b float64) bool {
+	return math.Abs(a-b) < this()
+}
+
+//>
+func (this Accuracy) Greater(a, b float64) bool {
+	return math.Max(a, b) == a && math.Abs(a-b) > this()
+}
+
+//<
+func (this Accuracy) Smaller(a, b float64) bool {
+	return math.Max(a, b) == b && math.Abs(a-b) > this()
+}
+
+//>=
+func (this Accuracy) GreaterOrEqual(a, b float64) bool {
+	return math.Max(a, b) == a || math.Abs(a-b) < this()
+}
+
+//<=
+func (this Accuracy) SmallerOrEqual(a, b float64) bool {
+	return math.Max(a, b) == b || math.Abs(a-b) < this()
 }
