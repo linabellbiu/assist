93bdd9db477f6efe8440c8047a4850e62524852a
xudong <834971685@qq.com>
2020-06-24 14:43:40 +0800

    更新

--- time.go
+++ time.go
@@ -1,10 +1,19 @@
 package assist
 
-import "time"
+import (
+	"time"
+)
 
 func TimeFormatToUnix(t string) int64 {
 	timeLayout := "2006-01-02 15:04:05"
 	loc, _ := time.LoadLocation("Local")
 	theTime, _ := time.ParseInLocation(timeLayout, t, loc) //使用模板在对应时区转化为time.time类型
 	return theTime.Unix()
+}
+
+func UnixToTimeFormat(t int64) string {
+	//获取本地location
+	timeLayout := "2006-01-02 15:04:05" //转化所需模板
+	//时间戳转日期
+	return time.Unix(t, 0).Format(timeLayout) //设置时间戳 使用模板格式化为日期字符串
 }
