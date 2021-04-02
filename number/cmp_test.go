5ad01b8135b2113d842cc298dd1c782951ebafde
wangxudong123 <834971685@qq.com>
2021-04-03 00:51:50 +0800

    更新文档

--- number/cmp_test.go
+++ number/cmp_test.go
@@ -1,26 +1,30 @@
 package number
 
 import (
-	"fmt"
 	"testing"
 )
 
+func TestCmp_Greater(t *testing.T) {
+	res := Cpm.Greater(3, 2)
+	t.Log(res)
+}
+
 func TestMax(t *testing.T) {
-	max := NewNumbers(3, 44, 5, 6, 6, 4, 9, 31, 23, 8, 3453, float64(4.999999999), float64(9.99999999), float64(2.0000001)).Max()
-	fmt.Println(max)
+	max := NewNumbers(3, 44, 3453, float64(4.999999999), float64(2.0000001)).Max()
+	t.Log(max)
 }
 
 func TestMin(t *testing.T) {
-	max := NewNumbers(3, 44, 5, 6, 6, 2, 4, 9, 31, 23, 8, 3453, float64(4.999999999), float64(9.99999999), float64(2.0000001)).Min()
-	fmt.Println(max)
+	min := NewNumbers(3, 44, 3453, float64(4.999999999), float64(2.0000001)).Min()
+	t.Log(min)
 }
 
 func TestNumber_OrderAsc(t *testing.T) {
-	asc := NewNumbers(3, 44, 5, 6, 6, 2, 4, 9, 31, 23, 8, 3453, float64(4.999999999), float64(9.99999999), float64(2.0000001)).OrderAsc()
-	fmt.Println(asc)
+	asc := NewNumbers(3, 44, 3453, float64(4.999999999), float64(2.0000001)).OrderAsc()
+	t.Log(asc)
 }
 
 func TestNumber_OrderDesc(t *testing.T) {
-	desc := NewNumbers(3, 44, 5, 6, 6, 2, 4, 9, 31, 23, 8, 3453, float64(4.999999999), float64(9.99999999), float64(2.0000001)).OrderDesc()
-	fmt.Println(desc)
+	desc := NewNumbers(3, 44, 3453, float64(4.999999999), float64(2.0000001)).OrderDesc()
+	t.Log(desc)
 }
